5e1c35a6200e7157afacdb41e3d8f9e4c552c598
ealopera <44577866+ealopera@users.noreply.github.com>
2021-03-03 12:37:20 +0100

    Add files via upload

---
+++ SAIGE/step1_null_model.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=3gb
+
+echo hostname
+echo "wkdir:" $1
+echo "phenotype=" $2
+echo "pheno_file=" $3
+echo "GRMfile=" $4
+echo "out=" $5
+echo "threads=" $6
+echo "covar" $7
+echo "removezero=" ${8}
+echo "singularityname=" ${9}
+echo "sample_col=" ${10}
+echo "trait_type=" ${11}
+
+
+##################################### step 1 ###################################################
+  mkdir -p ${1}/${5}
+  if [[ ${8} == "1" ]]; then
+      ### get the column nu2er for the specific trait
+      n_2=$( awk  -v col=${2} 'NR==1{for (i=1; i<=NF; i++) if ($i==col) {print i;exit}}' ${1}/${3}  )
+      ### create no_) file for the 3
+      awk  -F'\t'  '$n!=0 '  n=$n_2  ${1}/$3> ${1}/${5}/feat.temp
+    else
+      cat ${1}/{3} > ${1}/${5}/feat.temp
+  fi
+  
+  nsize=$( wc -l ${1}/${5}/feat.temp )
+  echo "number of non-NA samples =" ${nsize}
+  
+  singularity instance start -B  ${1}:/tmp /groups/umcg-lifelines/tmp01/projects/dag3_fecal_mgs/umcg-elopera/tools/saige_0.39.sif ${9}
+  ###launch step 1 (null model)
+  #For Quantitative traits, if not normally distributed, inverse normalization needs to be specified to be TRUE --invNormalize=TRUE
+  
+  if [[ ${11} != "binary"  ]]; then
+   
+  singularity exec instance://${9}  step1_fitNULLGLMM.R \
+          --plinkFile=/tmp/${4} \
+          --phenoFile=/tmp/${5}/feat.temp \
+          --phenoCol=${2} \
+          --covarColList=${7} \
+          --sampleIDColinphenoFile=${10} \
+          --traitType=${11}  \
+          --invNormalize=TRUE     \
+          --outputPrefix=/tmp/${5}/model \
+          --IsOverwriteVarianceRatioFile=TRUE \
+          --LOCO=FALSE    \
+          --nThreads=${6} \
+          --tauInit=1,0
+else
+	
+  singularity exec instance://${9}  step1_fitNULLGLMM.R \
+          --plinkFile=/tmp/${4} \
+          --phenoFile=/tmp/${5}/feat.temp \
+          --phenoCol=${2} \
+          --covarColList=${7} \
+          --sampleIDColinphenoFile=${10} \
+          --traitType=${11}  \
+          --outputPrefix=/tmp/${5}/model \
+          --IsOverwriteVarianceRatioFile=TRUE \
+          --LOCO=FALSE    \
+          --nThreads=${6} \
+          --tauInit=1,0
+fi
+  
+rm ${1}/${5}/feat.temp
+singularity instance stop ${9} 
+
+
+
---
+++ SAIGE/step2_assoc_only.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --mem=2gb
+
+echo hostname
+echo "wkdir:" $1
+echo  "testgenotypes=" $2
+echo "out=" $3
+echo "singularityname=" ${4}
+echo "chromosome:" ${5}
+echo "format:" ${6}
+
+##################################### step 2 ###################################################
+
+singularity instance start -B  ${1}:/tmp /groups/umcg-lifelines/tmp01/projects/dag3_fecal_mgs/umcg-elopera/tools/saige_0.39.sif ${4} 
+
+
+
+#####second step
+if [[ ${6} != "vcf"  ]]; then
+   
+  singularity exec instance://${4}  step2_SPAtests.R \
+    --bgenFile=/tmp/${2} \
+    --bgenFileIndex=/tmp/${2}.bgi \
+    --sampleFile=/tmp/${2}.sample \
+    --minMAF=0.01 \
+    --minInfo=0.4 \
+    --chrom=${5} \
+    --GMMATmodelFile=/tmp/${3}/model.rda \
+    --varianceRatioFile=/tmp/${3}/model.varianceRatio.txt \
+    --SAIGEOutputFile=/tmp/${3}/p-values.chr.${5} \
+    --IsOutputAFinCaseCtrl=FALSE \
+    --numLinesOutput=2 
+  else
+
+  singularity exec instance://${4}  step2_SPAtests.R \
+    --vcfFile=/tmp/${2} \
+    --vcfFileIndex=/tmp/${2}.csi \
+    --vcfField=DS \
+    --minMAF=0.01 \
+    --minInfo=0.4 \
+    --chrom=${5} \
+    --GMMATmodelFile=/tmp/${3}/model.rda \
+    --varianceRatioFile=/tmp/${3}/model.varianceRatio.txt \
+    --SAIGEOutputFile=/tmp/${3}/p-values.chr.${5} \
+    --IsOutputAFinCaseCtrl=FALSE \
+    --numLinesOutput=2 
+fi
+
+
+singularity instance stop ${4} 
+
+
+
